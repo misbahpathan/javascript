68ef5943cdfc433b6976a1c390661ddc10a57e5a
misbahpathan <126140582+misbahpathan@users.noreply.github.com>
2023-02-24 23:52:25 +0530

    Update index.js

--- function/index.js
+++ function/index.js
@@ -1,7 +1,27 @@
 //simple function
 
-
 function abc(name, lastname){
     console.log(`hello ${name} ${lastname}`);
 }
 abc('zainu', 'pathan');
+
+
+//function with array of object
+
+let abc = [{name : 'siraj', age : 30},
+            {name : 'misba', age : 26},
+            {name : 'zainu', age : 2},
+            {name : 'zoyu', age : 2}]
+
+            let abc1 = [{name : 'siraj1', age : 30},
+            {name : 'misba1', age : 26},
+            {name : 'zainu1', age : 2},
+            {name : 'zoyu1', age : 2}]
+
+function greet (arr){
+    for(let i=0;i<arr.length;i++){
+        console.log(arr[i].name, arr[i].age);
+    }
+}
+greet(abc);
+greet(abc1);
